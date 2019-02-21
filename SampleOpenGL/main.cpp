9e9efbc2c918aed1bdc4a4c5734d66f21be6f89a
SowatKheang <sowatkheang14@gmail.com>
2019-02-21 18:24:34 +0700

    initial commit

---
+++ SampleOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SampleOpenGL
+//
+//  Created by Sowat Kheang on 2/6/19.
+//  Copyright © 2019 Sowat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
